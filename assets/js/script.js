8075439d8bf7dced1c92ac014f8f790a489e6802
DenisDiachenko <sarmat4ever@gmail.com>
2018-09-20 14:41:21 +0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,7 +62,7 @@ const apiGetCurrentLocationRequest = (userPosition) => {
         .then(data => apiGetCurrentConditionsRequest(data[0]))
 }
 
-// window.onload = async function () {
+window.onload = function () {
     let userPosition;
     const geoOptions = {
         maximumAge: 5 * 60 * 1000,
@@ -78,4 +78,4 @@ const apiGetCurrentLocationRequest = (userPosition) => {
     navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions)
 
 
-// }
+}
